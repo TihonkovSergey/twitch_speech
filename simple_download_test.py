39c4ca334e570c8888e0fb110652a5a6d2425da2
TihonkovSergey <slezki.edinoroga@mail.ru>
2020-11-03 00:01:15 +0300

    Simple download test added

---
+++ simple_download_test.py
@@ -0,0 +1,24 @@
+from vod_download_utils.commands import download
+from copy import deepcopy
+
+vod_id = '574423677'
+
+default_args = {
+    "video": "1234567",
+    "quality": "worst",
+    "max_workers": 20,
+    "start": None,
+    "end": None,
+    "format": "mkv",
+    "keep": False,
+    "no_join": False,
+    "overwrite": True,
+    "path": None,
+    "filename": None
+}
+args = deepcopy(default_args)
+args["video"] = vod_id
+args["path"] = "data/"
+args["filename"] = vod_id
+
+download(args)
