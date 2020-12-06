929d43b03583a6c01fc97d3dd14dcc2fa693533f
TihonkovSergey <slezki.edinoroga@mail.ru>
2020-12-06 05:27:04 +0300

    config file with global paths

---
+++ config.py
@@ -0,0 +1,5 @@
+VODS_DIR_PATH = "/home/sergey/Documents/homeworks/twitch_speech/data/vods/"
+
+SOUNDS_DIR_PATH = "/home/sergey/Documents/homeworks/twitch_speech/data/sounds/"
+
+TMP_DIR_PATH = "/home/sergey/Documents/homeworks/twitch_speech/data/tmp/"
