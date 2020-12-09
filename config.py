1f771c74fa5734111955680a5188245f91f4e63e
TihonkovSergey <slezki.edinoroga@mail.ru>
2020-12-09 11:09:39 +0300

    fix subs path

--- config.py
+++ config.py
@@ -4,6 +4,8 @@ SOUNDS_DIR_PATH = "/home/sergey/Documents/homeworks/twitch_speech/data/sounds/"
 
 RECOGNITION_RESULTS_DIR_PATH = "/home/sergey/Documents/homeworks/twitch_speech/data/recognition_results/"
 
+SUBS_DIR_PATH = RECOGNITION_RESULTS_DIR_PATH + "ass/"
+
 TMP_DIR_PATH = "/home/sergey/Documents/homeworks/twitch_speech/data/tmp/"
 
 START_RECOGNITION_PATH = '/home/sergey/Documents/homeworks/twitch_speech/speech_to_text_russian/'
