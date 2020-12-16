7c3873aa92b346acd8f2273a827be75c0db20df0
alsvsl <alisa.vasilewa@yandex.com>
2020-12-16 04:49:58 +0300

    added index

---
+++ ui/index.js
@@ -0,0 +1,7 @@
+window.onload = function () {
+    const submitButton = document.getElementById('submit');
+    submitButton.addEventListener('click', async function () {
+        const id = document.getElementById('input').value.slice(29);
+        window.location.href = `http://localhost:1234/video.html?video_id=${id}`;
+    });
+}
